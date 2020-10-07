06cad78d7270310ce65644e572e83168785a8b58
dolinskaya <dolinskaya@tutu.tech>
2020-10-07 12:16:56 +0300

    fixup

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,17 +2,12 @@ const { getQueriesFrom } = require("@testing-library/nightwatch");
 
 module.exports = {
   "Demo test ecosia.org": async function (browser) {
-    browser.url("https://www.google.ru/");
+    browser.url("https://www.google.com/");
 
-    const { getAllByText } = getQueriesFrom(browser);
+    const { getAllByAltText } = getQueriesFrom(browser);
 
-    const { selector } = await getAllByText("Google");
+    const { selector } = await getAllByAltText("Google");
 
     console.log("--------selector---", selector);
-
-    // browser.setValue(
-    //   '[data-ved="0ahUKEwjEtoajptzrAhWMlIsKHS8IBGsQ39UDCAQ"]',
-    //   "hello world"
-    // );
   },
 };
